f65726d4633801762052f3b6c36a08e0540985fc
Rebeca Calazans <rebecacalazans@rebeca-linux.localdomain>
2015-10-26 13:43:08 -0200

    Criada função que adiciona rota ao route cache 1. A função pode adicionar uma rota qualquer a partir de um vetor de rotas 2. A função pode adicionar uma rota para o nó fonte de uma routerqt a partir da mesma

--- routediscovery.cpp
+++ routediscovery.cpp
@@ -1,8 +1,11 @@
-#include <netinet/ip.h>
 #include <dsr.h>
 #include <utils.h>
+#include <netinet/ip.h>
 #include <routediscovery.h>
 #include <arpa/inet.h>
+#include <time.h>
+
+#include <map>
 
 unsigned int create_routerqt (char *packet, unsigned int target, unsigned int saddr) {
   struct iphdr* ip = (struct iphdr*) packet;
@@ -53,4 +56,42 @@ unsigned int addaddr_routerqt (char *packet, unsigned int saddr) {
   unsigned int packet_len = sizeof(struct iphdr) + sizeof(struct routerqt_hdr) + routerqt->data_len - 6;
 
   return packet_len;
+}
+
+void addroute (std::map<unsigned int, struct route*> &routes, unsigned int* addrs, int n) {
+  struct route* ptr;
+  ptr = (struct route*) malloc (sizeof(struct route) + n * sizeof(unsigned int));
+  ptr->t = clock();
+  ptr->route_len = n;
+  ptr->hosts = (unsigned int*) (ptr + 1);
+  for (int i = 0; i < n; i++) {
+    ptr->hosts[i] = addrs[i];
+  }
+  routes[ptr->hosts[n-1]]=ptr;
+}
+
+//Essa função obtém, a partir da route request, a rota até o nó que enviou a requisição
+void addroute (std::map<unsigned int, struct route*> &routes, char *packet, unsigned int laddr) {
+  struct route* ptr;
+
+  struct routerqt_hdr *routerqt = (struct routerqt_hdr*) (packet + sizeof(struct iphdr));
+  //O ponteiro abaixo aponta para o primeiro endereço contido no pacote
+  unsigned int *addr = (unsigned int*) (packet + sizeof(struct iphdr) + sizeof(struct routerqt_hdr));
+  int pos = 0;
+  while (*addr != laddr) {
+    addr++;
+    pos++;
+  }
+  int n = pos + 1;
+
+  ptr = (struct route*) malloc (sizeof(struct route) + n * sizeof(unsigned int));
+  ptr->t = clock();
+  ptr->route_len = n;
+  ptr->hosts = (unsigned int*) (ptr + 1);
+
+  for (int i = 0; i < n; i++) {
+    ptr->hosts[i] = *addr;
+    addr--;
+  }
+  routes[ptr->hosts[n-1]]=ptr;
 }
--- routediscovery.h
+++ routediscovery.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <map>
 
 unsigned int create_routerqt (char *packet, unsigned int target, unsigned int saddr);
 unsigned int addaddr_routerqt (char *packet, unsigned int saddr);
+void addroute (std::map<unsigned int, struct route*> &routes, unsigned int* addrs, int n);
+void addroute (std::map<unsigned int, struct route*> &routes, char *packet, unsigned int laddr);
